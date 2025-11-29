309da8807bb35348afeb111b6507b16dcdc59016
hr <i.hrr.in@gmail.com>
2025-11-29 18:45:39 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -35,7 +35,7 @@ export const Navbar = () => {
           href="#hero"
         >
           <span className="relative z-10">
-            <span className="text-glow text-foreground"> Harnoor </span>{" "}
+            <span className="text-glow text-foreground"> HR's </span>{" "}
             Portfolio
           </span>
         </a>
